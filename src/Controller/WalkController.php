1081c44dc70d480e8558c72abc0f1596b6dfc558
Lucas-nmp <lucas.morandeira.parejo@gmail.com>
2025-03-10 12:26:15 +0100

    Restrict access to the route manager

--- src/Controller/WalkController.php
+++ src/Controller/WalkController.php
@@ -10,8 +10,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/walk')]
+#[IsGranted('ROLE_ADMIN')]
 final class WalkController extends AbstractController
 {
     #[Route(name: 'app_walk_index', methods: ['GET'])]
